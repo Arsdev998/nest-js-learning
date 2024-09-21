ce96e608b69928724420ea971177b7bfb74c03dc
Arsdev998 <arsdev998@gmail.com>
2024-09-21 14:31:35 +0700

    day-2 -cookie

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,17 +1,61 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Response,Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  //cookie set
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('succes set cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request) {
+    return request.cookies['name'];
+  }
+
+  // params and query http and async
   @Get('/hello')
-  getIdParams(
+  @HttpCode(200)
+  async getIdParams(
     @Query('first_name') firstName: string,
     @Query('last_name') lastName: string,
-  ): string {
+  ): Promise<string> {
     return `GET QUERY PARAMS NAME : ${firstName} ${lastName}`;
   }
-  
+
+  // sample response http
+  @Get('/response')
+  getResponse(@Res() res: Response): Response {
+    return res
+      .status(200)
+      .send('Hello World! ini response di nest js tapi ini not recomended');
+  }
+
+  // response http
+  @Get('/walawe')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  getWalawe(): Record<string, string> {
+    return {
+      data: 'wathehelll',
+      apaan: 'gatau',
+    };
+  }
+
   @Get('/:id')
-  getId(@Param("id") id:string): string {
+  getId(@Param('id') id: string): string {
     return `GET ${id} Userrr`;
   }
 
